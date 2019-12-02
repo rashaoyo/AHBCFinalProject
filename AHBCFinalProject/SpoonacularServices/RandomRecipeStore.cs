39876160423fe6ce495a45a9cf0bae4d8545067a
edwardsosnoski <edward.sosnoski@gmail.com>
2019-12-01 20:45:45 -0500

    added random recipe store

---
+++ AHBCFinalProject/SpoonacularServices/RandomRecipeStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AHBCFinalProject.Models;
+using Newtonsoft.Json;
+
+namespace AHBCFinalProject.SpoonacularServices
+{
+    public class RandomRecipeStore : IRandomRecipeStore
+    {
+        public async Task<ListOfRecipesResponse> GetRandomRecipe()
+        {
+            using(var httpClient = new HttpClient { BaseAddress = new Uri("spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/") })
+            {
+                httpClient.DefaultRequestHeaders.Add("x-rapidapi-host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
+                httpClient.DefaultRequestHeaders.Add("x-rapidapi-key", "a20e2189eamsh62909a90994a81ep16f226jsn158ad197afec");
+
+                var result = await httpClient.GetStringAsync("recipes/random/?number=7");
+                return JsonConvert.DeserializeObject<ListOfRecipesResponse>(result);
+            }
+        }
+    }
+}
