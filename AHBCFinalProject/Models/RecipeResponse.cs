431b97f308669b36fb8731fd7b701393156b3b7e
nick born <nicholasjborn@gmail.com>
2019-12-02 20:36:29 -0500

    json response

--- AHBCFinalProject/Models/RecipeResponse.cs
+++ AHBCFinalProject/Models/RecipeResponse.cs
@@ -5,33 +5,88 @@ namespace AHBCFinalProject.Models
 {
     public class RecipeResponse
     {
-        public bool Vegetarian { get; set; }
-        public bool Vegan { get; set; }
-        public bool GlutenFree { get; set; }
-        public bool DairyFree { get; set; }
-        public bool VeryHealthy { get; set; }
-        public bool Cheap { get; set; }
-        public bool VeryPopular { get; set; }
-        public bool Sustainable { get; set; }
-        public int WeightWatcherSmartPoints { get; set; }
-        public string Gaps { get; set; }
-        public bool LowFodmap { get; set; }
-        public bool Ketogenic { get; set; }
-        public bool Whole30 { get; set; }
-        public int Servings { get; set; }
-        public int PreparationMinutes { get; set; }
-        public int CookingMinutes { get; set; }
-        public string SourceUrl { get; set; }
-        public string SpoonacularSourceUrl { get; set; }
-        public int AggregateLikes { get; set; }
-        public string CreditText { get; set; }
-        public string SourceName { get; set; }
-        public List<IngredientResponse> ExtendedIngredients { get; set; }
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public int ReadyInMinutes { get; set; }
-        public string Image { get; set; }
-        public string ImageType { get; set; }
-        public string Instructions { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string image { get; set; }
+        public string imageType { get; set; }
+        public int servings { get; set; }
+        public int readyInMinutes { get; set; }
+        public string license { get; set; }
+        public string sourceName { get; set; }
+        public string sourceUrl { get; set; }
+        public string spoonacularSourceUrl { get; set; }
+        public int aggregateLikes { get; set; }
+        public float healthScore { get; set; }
+        public float spoonacularScore { get; set; }
+        public float pricePerServing { get; set; }
+        public object[] analyzedInstructions { get; set; }
+        public bool cheap { get; set; }
+        public string creditsText { get; set; }
+        public object[] cuisines { get; set; }
+        public bool dairyFree { get; set; }
+        public object[] diets { get; set; }
+        public string gaps { get; set; }
+        public bool glutenFree { get; set; }
+        public string instructions { get; set; }
+        public bool ketogenic { get; set; }
+        public bool lowFodmap { get; set; }
+        public object[] occasions { get; set; }
+        public bool sustainable { get; set; }
+        public bool vegan { get; set; }
+        public bool vegetarian { get; set; }
+        public bool veryHealthy { get; set; }
+        public bool veryPopular { get; set; }
+        public bool whole30 { get; set; }
+        public int weightWatcherSmartPoints { get; set; }
+        public string[] dishTypes { get; set; }
+        public Extendedingredient[] extendedIngredients { get; set; }
+        public Winepairing winePairing { get; set; }
     }
+
+    public class Winepairing
+    {
+        public object[] pairedWines { get; set; }
+        public string pairingText { get; set; }
+        public object[] productMatches { get; set; }
+    }
+
+    public class Extendedingredient
+    {
+        public string aisle { get; set; }
+        public float amount { get; set; }
+        public string consitency { get; set; }
+        public int id { get; set; }
+        public string image { get; set; }
+        public Measures measures { get; set; }
+        public string[] meta { get; set; }
+        public string[] metaInformation { get; set; }
+        public string name { get; set; }
+        public string original { get; set; }
+        public string originalName { get; set; }
+        public string originalString { get; set; }
+        public string unit { get; set; }
+    }
+
+    public class Measures
+    {
+        public Metric metric { get; set; }
+        public Us us { get; set; }
+    }
+
+    public class Metric
+    {
+        public float amount { get; set; }
+        public string unitLong { get; set; }
+        public string unitShort { get; set; }
+    }
+
+    public class Us
+    {
+        public float amount { get; set; }
+        public string unitLong { get; set; }
+        public string unitShort { get; set; }
+    }
+
+
+
 }
