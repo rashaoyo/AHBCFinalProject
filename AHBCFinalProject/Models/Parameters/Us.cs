a779936775d8e16ebf9b9b1faa7d6a67e2b38ca3
edwardsosnoski <edward.sosnoski@gmail.com>
2019-12-04 21:07:25 -0500

    added class files for RecipeResponse paramaters

---
+++ AHBCFinalProject/Models/Parameters/Us.cs
@@ -0,0 +1,9 @@
+﻿namespace AHBCFinalProject.Models
+{
+    public class Us
+    {
+        public float Amount { get; set; }
+        public string UnitLong { get; set; }
+        public string UnitShort { get; set; }
+    }
+}
---
+++ AHBCFinalProject/Models/Parameters/Winepairing.cs
@@ -0,0 +1,9 @@
+﻿namespace AHBCFinalProject.Models
+{
+    public class Winepairing
+    {
+        public object[] PairedWines { get; set; }
+        public string PairingText { get; set; }
+        public object[] ProductMatches { get; set; }
+    }
+}
