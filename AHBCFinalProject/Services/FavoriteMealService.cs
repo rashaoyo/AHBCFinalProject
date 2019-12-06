722956c020b8228274ad05a3b552091d45705a0e
nick born <nicholasjborn@gmail.com>
2019-12-06 15:13:28 -0500

    meal services added; not really functional atm

---
+++ AHBCFinalProject/Services/FavoriteMealService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AHBCFinalProject.DAL;
+using AHBCFinalProject.Models;
+
+namespace AHBCFinalProject.Services
+{
+    public class FavoriteMealService : IFavoriteMealService
+    {
+        private readonly IFavoriteMealsStore _favoriteMealStore; 
+        public FavoriteMealService(IFavoriteMealsStore favoriteMealStore)
+        {
+            _favoriteMealStore = favoriteMealStore;
+        }
+
+        public FavoriteMealViewModel DeleteAFavoriteMeal(int recipeId)
+        {
+        
+            throw new NotImplementedException();
+        }
+
+        public FavoriteMealViewModel InsertAFavoriteMeal(int recipeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RecipeViewModel SelectAFavoriteMeal(int recipeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FavoriteMealViewModel SelectAllFavoriteMeals()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
